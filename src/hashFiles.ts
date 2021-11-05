5343e5b9ea8aaa7d47e175f65714764540285e14
SungHun Moon <seunghunmoon023@gene1on1.com>
2021-11-05 10:58:33 +0900

    feat: Keep the number of files and pass it to the child thread
    Keep it since you must finish the all threads after processing all files.

--- src/hashFiles.ts
+++ src/hashFiles.ts
@@ -1,13 +1,26 @@
-import {parentPort} from "worker_threads";
+import {parentPort, Worker, workerData} from "worker_threads";
 import {createHash} from "crypto";
+import path from "path";
 // Hash class is a utility for creating hash digests of data.
 
+// The hashFile thread should keep and share the file list object with removeFiles thread
+// [
+//  {name: fileName, value: hashedValue},
+//  {name: fileName, value: hashedValue},
+// ...
+// ]
+
+// you should pass 'workerData' in constructor even though parentThread already had it.
+const removeFileWorker = new Worker(path.join(__dirname, 'removeFiles.ts'), {workerData: {numOfFile: workerData.numOfFile}});
 
 parentPort.on('message', (fileObj: any)=>{
     const hashGenerator = createHash('MD5');
     hashGenerator.update(fileObj.value);
     const hashedValue : string = hashGenerator.digest('hex');
 
-    console.log(fileObj.name)
-    console.log(hashedValue)
-})
+    console.log(fileObj.name);
+    console.log(hashedValue);
+
+    removeFileWorker.postMessage({name: fileObj.name, value: hashedValue});
+});
+
--- src/readFiles.ts
+++ src/readFiles.ts
@@ -1,36 +1,32 @@
-import {Worker, parentPort} from "worker_threads";
-import {readFile, readdir} from 'fs/promises';
+import {Worker} from "worker_threads";
+import {readFile} from 'fs/promises';
 import path from "path";
+import {readdirSync} from "fs";
 
 // 2. hashing file and store {filename: hashed value}
 const filePath = path.join(__dirname, '..', 'example-images');
+const files = readdirSync(filePath, {encoding : 'utf-8', withFileTypes: true});
+const numOfFiles : number = files.length;
+console.log(`파일 수 : ${numOfFiles}`);
 
-const hashFileWorker = new Worker(path.join(__dirname, 'hashFiles.ts'));
+// 총 파일 수는 넘겨야함.
+const hashFileWorker = new Worker(path.join(__dirname, 'hashFiles.ts'), {workerData: {numOfFile: numOfFiles}});
 
-(async ()=>{
-
-    try {
-        const files = await readdir(filePath, {encoding : 'utf-8', withFileTypes: true});
-        const numOfFiles : number = files.length;
-        console.log(`파일 수 : ${numOfFiles}`);
-
-        // 파일을 다 읽고 넘기는게 아니라 읽을 때마다 족족 넘겨야함.
-
-        for (const file of files) {
-            readFile(path.join(filePath, file.name))
-                // 여기서 족족 넘겨야함.
-                .then((value: Buffer)=>{
-                    hashFileWorker.postMessage({name: file.name, value: value});
-                })
-                .catch((reason: string)=>{
-                    console.error({name: file.name, reason: reason});
-                });
-        }
-
-    } catch (error) {
-        console.error(error)
+try {
+    // 파일을 다 읽고 넘기는게 아니라 읽을 때마다 족족 넘겨야함.
+    for (const file of files) {
+        readFile(path.join(filePath, file.name))
+            // 여기서 족족 넘겨야함.
+            .then((value: Buffer)=>{
+                hashFileWorker.postMessage({name: file.name, value: value});
+            })
+            .catch((reason: string)=>{
+                console.error({name: file.name, reason: reason});
+            });
     }
+} catch (error) {
+    console.error(error)
+}
 
-})();
 
 
